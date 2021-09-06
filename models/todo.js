0f3c87357c271b91075785a5993abd26842415ec
Igor Milosevic <igor.milosevic1988@gmail.com>
2021-09-06 10:22:41 +0200

    creates post route so todos can be added

--- models/todo.js
+++ models/todo.js
@@ -2,16 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const todoSchema = new Schema({
- id: {
-  type: String,
-  required: true
- },
  todo: {
   type: String,
   required: true
  },
  date: {
-  type: String,
+  type: Date,
   required: true
  }
 });
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const cors = require('cors');
+router.use(cors());
 const mongoose = require('mongoose');
+const Todo = require('../models/todo')
 
-// Connecting to my mongodb
+// Anslutning till mongo db
 const dbURI = 'mongodb+srv://dbUser:Rada192270@calendar-todo-app.cgmxw.mongodb.net/calendar-todo?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => console.log('uppkopplad mot databasen')) // Kan behöva lägga in listen på servern istället för console.log så att man inte gör anrop innan db laddas
   .catch((err) => console.log(err));
+
+
+// Post router för nya todos
+router.post('/add', (req, res, next) => {
+  console.log(req.body);
+  // Skapa en ny todo enligt modellen i mongoose
+  const todo = new Todo({
+    todo: req.body.todo,
+    date: req.body.date
+  })
+  // Sparar todo till db
+  todo.save().then((result) => {
+    console.log(result);
+    res.status(201).json({
+      message: 'todo sparad'
+    })
+  })
+})
 
 
 
